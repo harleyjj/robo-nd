47c0e744b94eff5d87c2e0373f22d98b232a4126
Harley Jackson <hjackson@linkedin.com>
2020-09-18 01:17:38 -0700

    Project 1 Submission

---
+++ Project1/script/welcome_message.cpp
@@ -0,0 +1,20 @@
+#include <gazebo/gazebo.hh>
+
+namespace gazebo
+
+{
+	class WorldPluginMyRobot : public WorldPlugin
+	{
+		public: WorldPluginMyRobot() : WorldPlugin()
+		{
+			printf("Welcome to Harley's World!\n");
+		}
+
+		public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
+		{
+			
+		}
+	};
+	GZ_REGISTER_WORLD_PLUGIN(WorldPluginMyRobot)
+
+}
